a621464f7b38db3a9fe1e43a46c792307387a171
benjaminsinaiko <ben.jamin@outlook.com>
2018-04-12 19:42:32 -0500

    109 - Add test for attachment file

--- spec/mailers/welcome_mailer_spec.rb
+++ spec/mailers/welcome_mailer_spec.rb
@@ -5,16 +5,24 @@ RSpec.describe WelcomeMailer, :type => :mailer do
     let(:lead) { build(:lead)}
     let(:mail) { WelcomeMailer.welcome_email(lead) }
 
-    it 'renders the subject' do
+    it 'should have a valid lead' do
+      expect(lead).to be_valid
+    end 
+
+    it "sends a welcome email to the lead's email address" do
+      expect(mail.to).to eql(['jdoe@email.com'])
+    end
+
+    it "sends from the admissions email address" do
+      expect(mail.from).to eq(['admissions@actualize.co'])
+    end
+
+    it 'sends with the correct subject' do
       expect(mail.subject).to eql('Welcome to Actualize!')
     end
 
-    it "renders the sender email" do
-      expect(mail.from).to eq(["admissions@actualize.co"])
-    end
-
-    it 'renders the receiver email' do
-      expect(mail.to).to eql(['jdoe@email.com'])
+    it 'sends correct attachment' do
+      expect(mail.attachments[0].filename).to include('welcome-email-image.jpg')
     end
 
   end
